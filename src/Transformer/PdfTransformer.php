05c8140035ae0ddb9d9e6d3b8b40f73325eda803
santeacademie <julien.petit@santeacademie.com>
2021-12-13 07:49:04 -0600

    Add Ghostscript wrapper for better compressed file quality

--- src/Transformer/PdfTransformer.php
+++ src/Transformer/PdfTransformer.php
@@ -2,17 +2,18 @@
 
 namespace Santeacademie\SuperUploaderBundle\Transformer;
 
-use Imagick;
-use ImagickException;
+use Exception;
+use Org_Heigl\Ghostscript\Ghostscript;
 use Santeacademie\SuperUploaderBundle\Asset\Variant\AbstractVariant;
 use Santeacademie\SuperUploaderBundle\Asset\Variant\PdfVariant;
+use Santeacademie\SuperUploaderBundle\Ghostscript\Device\Pdf;
 use Santeacademie\SuperUploaderBundle\Interface\VariantTansformerInterface;
 use Symfony\Component\HttpFoundation\File\File;
 
 class PdfTransformer implements VariantTansformerInterface
 {
     /**
-     * @throws ImagickException
+     * @throws Exception
      */
     public function transformFile(File $file, PdfVariant|AbstractVariant $variant, array $variantTypeData): File
     {
@@ -20,10 +21,28 @@ class PdfTransformer implements VariantTansformerInterface
             $file->guessExtension() !== PdfVariant::EXTENSION
             || (!is_null($variant->getSizeLimit()) && $file->getSize() > $variant->getSizeLimit())
         ) {
-            $imagick = new Imagick($file->getRealPath());
+            $gs = new Ghostscript();
+            $gs->setDevice(new Pdf());
 
-            $imagick->setImageFormat(PdfVariant::EXTENSION);
-            $imagick->writeImage($file->getPathname());
+            $gs->setInputFile($file->getRealPath());
+
+            $compressedFile = sprintf('%s_compressed.%s',
+                $file->getBasename('.'.$file->getExtension()),
+                $file->getExtension()
+            );
+
+            $gs->setOutputFile($compressedFile);
+
+            if ($gs->render()) {
+                file_put_contents($file->getRealPath(), file_get_contents($file->getPath().'/'.$compressedFile));
+            } else {
+                throw new Exception(
+                    sprintf(
+                        'Ghostscript error: render process has failed to write compressed file: "%s"',
+                        $compressedFile
+                    )
+                );
+            }
         }
 
         return $file;
